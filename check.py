51789ab7cbf03dcf3c8f6044674c213054e1ba7c
yaki29 <yashjain.lnm@gmail.com>
2017-01-06 22:58:51 +0530

    haarcascade added

--- check.py
+++ check.py
@@ -2,13 +2,16 @@ import cv2
 import numpy as np 
 
 image1 = cv2.imread("1.jpg")
-image2 = cv2.imread("2.jpg")
+image2 = cv2.imread("4.jpg")
 
 difference = cv2.subtract(image1, image2)
 
 result = not np.any(difference)
+print difference
+print result
 
 if result is True:
 	print "Images are same"
+
 else:
 	print "Images are different"
--- main.py
+++ main.py
@@ -2,15 +2,18 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
-fgbg = cv2.createBackgroundSubtractorMOG2()
+smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 while(True):
     ret, frame = cap.read()
-    fgmask = fgbg.apply(frame)
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
- 
+    smile = smile_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x,y,w,h) in smile:
+		cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
+
+
     cv2.imshow('frame',frame)
-    cv2.imshow('fg', fgmask)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
