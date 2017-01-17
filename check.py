7bd783b429346cfa3bd9b1f7165b9c9e0b3315b6
yaki29 <yashjain.lnm@gmail.com>
2017-01-18 05:09:13 +0530

    condition custamized

--- check.py
+++ check.py
@@ -3,35 +3,18 @@ import cv2
 import os
 from kivy.uix.widget import Widget 
 from kivy.event import EventDispatcher
-from kivy.uix.behaviors import ButtonBehavior
-from kivy.properties import OptionProperty, ObjectProperty, NumericProperty, \
-    ReferenceListProperty, BooleanProperty, ListProperty, AliasProperty, \
-    StringProperty
+from kivy.properties import NumericProperty, StringProperty
 
 
-
-# class P(BoxLayout):
-
-#     def __init__(sef, **kwargs):
-#         super(P, self).__init__(**kwargs)
-#         self.register_event_type('on_match')
-
-
-#     def on_match(self):
-#         screen_manager.current = 'next_creen'
-
-#     def on_press(self):
-#         print "sidhciusdcius"
 
 
 class FaceLock(Widget):
     cascade = StringProperty()
 
     index = NumericProperty(0)
-    print "Not called"
-    __events__ = ('on_brick_breacked', )
-    def on_brick_breacked(instance, *args):
-        pass
+
+    
+    
     def __init__(self, **kwargs):
         self.register_event_type('on_match')
         super(FaceLock, self).__init__(**kwargs)
@@ -41,29 +24,27 @@ class FaceLock(Widget):
         pass
     def face_recognize(self):
         cap = cv2.VideoCapture(self.index)
-        smile_cascade = cv2.CascadeClassifier(self.cascade)
+        face_cascade = cv2.CascadeClassifier(self.cascade)
         while(True):
             ret, frame = cap.read()
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            if np.any(smile_cascade.detectMultiScale(gray, 1.3, 5)):
-                smile = smile_cascade.detectMultiScale(gray, 1.3, 5)
-                print "hand found"
+            if np.any(face_cascade.detectMultiScale(gray, 1.3, 5)):
+                
+                print ("Cascade found")
+                
                 self.dispatch('on_match')
-                print "after dispatch"
+                
                 cv2.destroyAllWindows()
                 for i in range(1,5):
                     cv2.waitKey(1)
                 break
-                for (x,y,w,h) in smile:
-                    cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
             else:
-                print "Lost it"
+                print ("Not recognized")
 
             cv2.imshow('frame',frame)
-            print "yash"
             
             if cv2.waitKey(1) & 0xFF == ord('q'):
-                print "hey q pressed"
+                print ("Forcefully Closed")
                 
                 cv2.destroyAllWindows()
                 for i in range(1,5):
--- example.py
+++ example.py
@@ -4,43 +4,38 @@ from kivy.lang import Builder
 from kivy.uix.label import Label 
 from kivy.uix.screenmanager import Screen, ScreenManager, FadeTransition
 Builder.load_string('''
-<StartScreen>:
-	BoxLayout:
-		orientation: 'vertical'
-		Label:
-			id: lbl
-			text: "You have a nice face !!!"
-			font_size: 40
-<LockScreen>:
-	
-	index: 0
-	cascade: 'haarcascade_frontalface_default.xml'
-	on_match: root.change_scr()
-	BoxLayout: 
-		Label: 
-			text: "Same Screen"
-			font_size: 40
-		Button:
-			text: "change"
-			on_press: root.manager.current = "application_screen"
 
+<LockScreen>:
+    
+    index: 0
+    cascade: 'haarcascade_frontalface_default.xml'
+    on_match: root.change_scr()
+    BoxLayout:
+        id: bl
+        Label:
+            text: "You have a nice face !!!"
+            font_size: 40
+        
 ''')
 
 
 class StartScreen(Screen):
-	pass
+    pass
 class LockScreen(FaceLock, Screen):
-	def change_scr(self):
-		print "Screen Changing" 
-		sm.current = "application_screen"
+    def change_scr(self):
+        print "You have unlocked the Application"
+        print "Secret Code: 15UCS164"
+        print "You can not access this code unless you unlock it"
+        print "You can do whatever that you want but it would need your face"
+
+
 
 sm = ScreenManager(transition=FadeTransition())
-sm.add_widget(StartScreen(name="application_screen"))
 sm.add_widget(LockScreen(name="lock_screen"))
 
 class CameraApp(App):
-	sm.current = "lock_screen"
-	def build(self):
-		return sm
+    
+    def build(self):
+        return sm
 
 CameraApp().run()
