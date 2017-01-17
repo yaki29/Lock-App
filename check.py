5c0d37981b5a1ebbc454af2225c90fb26b87fe0d
yaki29 <yashjain.lnm@gmail.com>
2017-01-18 05:23:35 +0530

    pep8 fixes

--- check.py
+++ check.py
@@ -1,11 +1,9 @@
 import numpy as np
 import cv2
 import os
-from kivy.uix.widget import Widget 
+from kivy.uix.widget import Widget
 from kivy.event import EventDispatcher
 from kivy.properties import NumericProperty, StringProperty
-
-
 
 
 class FaceLock(Widget):
@@ -13,8 +11,6 @@ class FaceLock(Widget):
 
     index = NumericProperty(0)
 
-    
-    
     def __init__(self, **kwargs):
         self.register_event_type('on_match')
         super(FaceLock, self).__init__(**kwargs)
@@ -22,6 +18,7 @@ class FaceLock(Widget):
 
     def on_match(self):
         pass
+
     def face_recognize(self):
         cap = cv2.VideoCapture(self.index)
         face_cascade = cv2.CascadeClassifier(self.cascade)
@@ -29,25 +26,22 @@ class FaceLock(Widget):
             ret, frame = cap.read()
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             if np.any(face_cascade.detectMultiScale(gray, 1.3, 5)):
-                
                 print ("Cascade found")
-                
                 self.dispatch('on_match')
-                
                 cv2.destroyAllWindows()
-                for i in range(1,5):
+                for i in range(1, 5):
                     cv2.waitKey(1)
                 break
             else:
                 print ("Not recognized")
 
-            cv2.imshow('frame',frame)
-            
+            cv2.imshow('frame', frame)
+
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 print ("Forcefully Closed")
-                
+
                 cv2.destroyAllWindows()
-                for i in range(1,5):
+                for i in range(1, 5):
                     cv2.waitKey(1)
                 break
         cap.release()
--- example.py
+++ example.py
@@ -1,12 +1,12 @@
 from check import FaceLock
-from kivy.app import App 
+from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.label import Label 
+from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen, ScreenManager, FadeTransition
 Builder.load_string('''
 
 <LockScreen>:
-    
+
     index: 0
     cascade: 'haarcascade_frontalface_default.xml'
     on_match: root.change_scr()
@@ -15,12 +15,14 @@ Builder.load_string('''
         Label:
             text: "You have a nice face !!!"
             font_size: 40
-        
+
 ''')
 
 
 class StartScreen(Screen):
     pass
+
+
 class LockScreen(FaceLock, Screen):
     def change_scr(self):
         print "You have unlocked the Application"
@@ -28,14 +30,14 @@ class LockScreen(FaceLock, Screen):
         print "You can not access this code unless you unlock it"
         print "You can do whatever that you want but it would need your face"
 
-
-
 sm = ScreenManager(transition=FadeTransition())
 sm.add_widget(LockScreen(name="lock_screen"))
 
+
 class CameraApp(App):
-    
+
     def build(self):
         return sm
+
 
 CameraApp().run()
